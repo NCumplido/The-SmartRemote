54fe7dbd5b8394c574fa05b29d424a1549695d91
Nachodood <ncumplido@msn.com>
2020-07-05 16:33:57 +0100

    Add DB attributes to Device class

--- app/src/main/java/nich/project/thesmartremote/Device.java
+++ app/src/main/java/nich/project/thesmartremote/Device.java
@@ -1,16 +1,31 @@
 package nich.project.thesmartremote;
 
+/*
+Some code from:
+
+Dr. Tom Owen: CSC306
+Swansea University
+ */
+
 public class Device {
 
+    // Labels table name
+    public static final String TABLE = "Device";
 
+    // Labels Table Columns names
+    public static final String KEY_ID = "id";
+    public static final String KEY_name = "name";
+    public static final String KEY_bearing = "bearing";
+
+    // property help us to keep data
+    public int device_ID;
+    public String name;
+    public int bearing;
 
     public Device(String name, int bearing) {
         this.name = name;
         this.bearing = bearing;
     }
-
-    private String name;
-    private int bearing;
 
     public String getName() {
         return name;
